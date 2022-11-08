2d0cfc168ed0cea0141383cf597339754597afe8
kamidate <s.kamidate.sys22@morijyobi.ac.jp>
2022-11-08 15:22:23 +0900

    D«Èm0¬ìǋL

--- Main.java
+++ Main.java
@@ -2,11 +2,13 @@ import java.util.*;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+    for(int i = 0; i < 2 ; i++){
     System.out.print("・身長(m)を入力してください：");
     double height = sc.nextDouble();
     System.out.print("・体重(Kg)を入力してください：");
     double weight = sc.nextDouble();
     double bmi = weight / (height*height);
     System.out.println("BMI値は"+bmi+"です。");
+    }
   }
 }
